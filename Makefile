fc53e72adac2c77b6b47b987d45db1f0e3eab92c
Oliver Pinel <oliver.debankle@gmail.com>
2019-02-15 16:48:27 +1000

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,28 @@
+ASMFILES 	:= 	main.asm
+OBJFILES 	:= 	main.o
+TARGET   	:= 	main
+
+NASM		:= 	nasm
+LD			:= 	ld
+CC			:= 	gcc-8
+
+NASMFLAGS	:= 	-f macho64
+GTKFLAGS 	:= 	-L/usr/local/Cellar/gtk+3/3.24.5/lib -lgtk-3 -lgobject-2.0
+LDFLAGS		:= 	-no_pie -lSystem -lc /usr/lib/crt1.o $(GTKFLAGS)
+CCFLAGS		:= 	`pkg-config --cflags --libs gtk+-3.0`
+
+all: link
+
+.PHONY: all link clean
+
+link: $(OBJFILES)
+	$(LD) $(LDFLAGS) -o $(TARGET) $^
+
+%.o: %.asm
+	$(NASM) $(NASMFLAGS) -o $@ $<
+
+%.o: %.c
+	$(CC) -c -fno-asynchronous-unwind-tables -O2 $(CCFLAGS) -o $@ $<
+
+clean:
+	rm -rf $(OBJFILES) $(TARGET)
---
+++ main.asm
@@ -0,0 +1,57 @@
+global _main
+
+%include "gtk.inc"
+
+section .bss
+    window:     resq 1
+
+
+section .rodata
+    signal:
+    .destroy:   db  "destroy", 0
+
+    title:      db  "My GTK+3 Window!", 0
+
+
+section .text
+
+
+    _destroy_window:
+        jmp _gtk_main_quit
+
+    _main:
+        push rbp
+        mov rbp, rsp
+
+        xor rdi, rdi
+        xor rsi, rsi
+        call _gtk_init
+
+        xor rdi, rdi
+        call _gtk_window_new
+        mov qword [ rel window ], rax
+
+        mov rdi, qword [ rel window ]
+        mov rsi, title
+        call _gtk_window_set_title
+
+        mov rdi, qword [ rel window ]
+        mov rsi, GTK_WIN_WIDTH
+        mov rdx, GTK_WIN_HEIGHT
+        call _gtk_window_set_default_size
+
+        mov rdi, qword [ rel window ]
+        mov rsi, GTK_WIN_POS_CENTER
+        call _gtk_window_set_position
+
+        mov rdi, qword [ rel window ]
+        mov rsi, signal.destroy
+        mov rdx, _destroy_window
+        xor rcx, rcx
+        xor r8d, r8d
+        xor r9d, r9d
+        call _g_signal_connect_data
+
+        call _gtk_main
+
+        call _exit
